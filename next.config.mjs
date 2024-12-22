2a2a7b932c3151b0f776978ce4bf65de5ca28066
Vojtěch Tranta <vojta.tranta@gmail.com>
2024-12-22 21:13:08 +0100

    put curretly rewirtes to cold

--- next.config.mjs
+++ next.config.mjs
@@ -7,20 +7,6 @@ const nextConfig = withNextIntl({
   experimental: {
     reactCompiler: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/:locale/:path*", // Matches dynamic locale and additional paths
-        has: [
-          {
-            type: "host",
-            value: "app.aistein.cz", // Matches only the app subdomain
-          },
-        ],
-        destination: "/app/:locale/:path*", // Maps to the app folder dynamically
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
