b6891d58cca4b1887129b35f7da578242056d572
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-01-13 15:40:57 +0100

    declare env vars in dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,18 @@ const nextConfig = withNextIntl({
       bodySizeLimit: "20mb",
     },
   },
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
+    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    LANGTAIL_API_KEY: process.env.LANGTAIL_API_KEY,
+    PINECONE_API_KEY: process.env.PINECONE_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 });
 
 export default nextConfig;
